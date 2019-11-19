4ba1803648504d99df38085c413df754337b80ec
Wesley Araujo de Brito <43002683+wesleydbrito@users.noreply.github.com>
2019-11-19 19:23:45 -0300

    Index update
    Adição da section serviços

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+jQuery(function($) {'use strict',
+
+
+		  
+	  //Window Loaded Handler
+
+  $(window).load(function(){'use strict';
+		 $(".loader").fadeOut("slow");
+		 
+  });
+// Scroll One Page Menu
+  $('a.page-scroll, .cbp-spmenu a').on('click', function(event){
+        //var $nav_height = $('#navigation.affix').outerHeight();		
+		var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1200, 'easeInOutExpo');
+       event.preventDefault();;
+		$('#navigation').affix({offset: {top: 50} });
+		
+		
+   });
